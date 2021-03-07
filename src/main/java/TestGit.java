f2a614ed6df11da794689f6bd1eab90f77c92e06
dph <1780028113@qq.com>
2021-03-07 11:27:36 +0800

    dph第三次上传

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -5,4 +5,28 @@ public class TestGit {
     private String date;
 
     private String name;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
