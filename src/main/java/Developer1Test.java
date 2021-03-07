6353f461486691e1de30a7cbe5e94029cfefb1d9
dph <1780028113@qq.com>
2021-03-07 12:12:23 +0800

    开发员1提交测试三

--- src/main/java/Developer1Test.java
+++ src/main/java/Developer1Test.java
@@ -1,4 +1,22 @@
 public class Developer1Test {
 
     String developer = "张三";
+
+    int age = 18;
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
